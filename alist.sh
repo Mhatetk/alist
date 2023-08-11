6fab2d883c7e4db8658e0aaba91dd55bc55934a3
Mhatetk <admin@mhatetk.top>
2023-08-11 10:13:20 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -4,28 +4,45 @@ mkdir -p /opt/alist/data/
 
 cat >/opt/alist/data/config.json <<EOF
 {
-  "address": "0.0.0.0",
-  "port": 2233,
-  "assets": "local",
+  "force": false,
+  "site_url": "https://pan.mhatetk.top",
+  "cdn": "https://registry.npmmirror.com/alist-web/$version/files/dist/",
+  "jwt_secret": "",
+  "token_expires_in": 48,
   "database": {
     "type": "$DATABASE",
-    "user": "$SQLUSER",
-    "password": "$SQLPASSWORD",
     "host": "$SQLHOST",
     "port": $SQLPORT,
+    "user": "$SQLUSER",
+    "password": "$SQLPASSWORD",
     "name": "$SQLNAME",
+    "db_file": "data\\data.db",
     "table_prefix": "x_",
-    "db_file": "data.db"
+    "ssl_mode": "require"
   },
   "scheme": {
-    "https": false,
+    "address": "0.0.0.0",
+    "http_port": 2233,
+    "https_port": -1,
+    "force_https": false,
     "cert_file": "",
-    "key_file": ""
+    "key_file": "",
+    "unix_file": "",
+    "unix_file_perm": ""
   },
-  "cache": {
-    "expiration": 60,
-    "cleanup_interval": 120
-  }
+  "temp_dir": "data\\temp",
+  "bleve_dir": "data\\bleve",
+  "log": {
+    "enable": true,
+    "name": "data\\log\\log.log",
+    "max_size": 10,
+    "max_backups": 5,
+    "max_age": 28,
+    "compress": false
+  },
+  "delayed_start": 0,
+  "max_connections": 0,
+  "tls_insecure_skip_verify": true
 }
 EOF
 
